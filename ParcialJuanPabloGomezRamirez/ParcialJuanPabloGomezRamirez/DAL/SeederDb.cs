8175f6e13b9cf5a53e7c0f7d3616df1b39fbe8fd
jPabloGomez1913 <Juangomezramirez527@gmail.com>
2023-05-21 22:52:33 -0500

    Added seeder

---
+++ ParcialJuanPabloGomezRamirez/ParcialJuanPabloGomezRamirez/DAL/SeederDb.cs
@@ -0,0 +1,39 @@
+﻿using ParcialJuanPabloGomezRamirez.DAL.Entities;
+
+namespace ParcialJuanPabloGomezRamirez.DAL
+{
+    public class SeederDb
+    {
+        private readonly DataBaseContext _context;
+        public SeederDb(DataBaseContext context)
+        {
+            _context= context;
+        }
+
+        public  async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await PopulateTicketsAsync();
+        }
+
+        private async Task PopulateTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for (int i = 1; i <=100; i++)
+                {
+                    _context.Tickets.Add(new Ticket
+                    {
+                        UseDate = null,
+                        IsUsed = false,
+                        EntranceGate = null,
+                    });
+                }
+               
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
